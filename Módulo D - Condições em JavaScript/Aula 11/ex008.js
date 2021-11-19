6d38f4b62ebf215964088c0f457122510fbeda38
TiagoTNX <tiagotnx@gmail.com>
2021-11-19 00:02:07 -0300

    Módulo D - Condições em JavaScript
    Aula 11

---
+++ Módulo D - Condições em JavaScript/Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { //vai mostrar a mensagem no console.log se o valor de vel for maior que 60
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ Módulo D - Condições em JavaScript/Aula 11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'//a variavel recebe o pais
+console.log(`Vivendo em ${país}`) //exibe esta mensagem na tela com template string 
+if (país == 'Brasil') {//se o país for igual a Brasil a primeira mensagem será apresentada no console.log
+    console.log('Você é Brasileiro!')
+} else {//se o país for diferente de Brasil a segunda mensagem será apresentada no console.log
+    console.log('Você é Estrangeiro!')
+}
