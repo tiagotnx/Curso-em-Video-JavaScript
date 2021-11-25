dc3f36317513c027dd836f326c3e063d88964e70
TiagoTNX <tiagotnx@gmail.com>
2021-11-25 12:56:53 -0300

    Exercício

--- Módulo E - Repetições em JavaScript/Exercícios JavaScript/ex016/script.js
+++ Módulo E - Repetições em JavaScript/Exercícios JavaScript/ex016/script.js
@@ -8,8 +8,33 @@ function contar() {
   var fimV = Number(fim.value);
   var passoV = Number(passo.value);
 
-  for (inicioV; inicioV <= fimV; inicioV = inicioV + passoV) {
-    res.innerHTML += `${inicioV} ->`;
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML = 'Impossível contar!'
+  } else {
+    if (passoV <= 0) {
+      alert('Passo inválido! Considerando PASSO 1')
+      passoV = 1
+    }
+    if (inicioV < fimV) {
+      res.innerHTML = 'Contando: <br>'
+      for (inicioV; inicioV <= fimV; inicioV = inicioV + passoV) { //contagem crescente
+        res.innerHTML += `${inicioV} \u{1f449}`;
+      }
+    } else {
+      res.innerHTML = 'Contando: '
+      for (inicioV; inicioV >= fimV; inicioV = inicioV - passoV) {//contagem regreciva
+        res.innerHTML += `${inicioV} \u{1f449}`;
+      }
+    }
+    res.innerHTML += `\u{1f3c1}`;
   }
-  res.innerHTML += "Fim";
+
+  // for (inicioV; inicioV <= fimV; inicioV = inicioV + passoV) {
+  //   res.innerHTML += `${inicioV} ->`;
+  // }
+  // res.innerHTML += "Fim";
 }
